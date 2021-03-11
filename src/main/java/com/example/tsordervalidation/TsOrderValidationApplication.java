8315894031022161be08254b489aa8df6bd0bee3
penbosso <ckbosso@st.ug.edu.gh>
2021-03-11 15:21:45 +0000

    initial setup

---
+++ src/main/java/com/example/tsordervalidation/TsOrderValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsordervalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsOrderValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsOrderValidationApplication.class, args);
+	}
+
+}
